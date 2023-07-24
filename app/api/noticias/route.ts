44f18751c4415f36902647817986de657fc1e4d6
matiasvidela96 <matividela@yahoo.com.ar>
2023-07-24 16:57:02 -0300

    wip: api noticias alphavantage

--- app/api/noticias/route.ts
+++ app/api/noticias/route.ts
@@ -1,6 +1,28 @@
 import { NextResponse } from "next/server"
+import dotenv from 'dotenv';
+
+// // Configura dotenv para cargar las variables de entorno desde el archivo .env
+dotenv.config();
+
 export async function GET(request: Request) {
-    const data = await fetch("https://www.freetogame.com/api/games");
-    const response = await data.json();
-    return NextResponse.json(response);
+
+    const yourApiKey = process.env.API_KEY || '';
+
+    const alpha = require('alphavantage')({ key: yourApiKey });
+
+    try {
+        // const data = await alpha.data.intraday(`msft`); 
+        const data = await alpha.experimental('NEWS_SENTIMENT');
+
+        return NextResponse.json(data);
+    } catch (err) {
+        console.error(err);
+        return NextResponse.error();
+    }
 }
+
+
+
+
+
+
--- app/noticias/page.tsx
+++ app/noticias/page.tsx
@@ -29,5 +29,4 @@ export default function Noticias() {
       </div>
     </section >
   );
-
 }
