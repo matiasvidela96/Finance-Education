9f9a06ec66478477ae952bf676c65a62f818355b
Ivo196 <ivo.tonioni@gmail.com>
2023-08-05 22:50:15 +0200

    fix to mobile view

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 function main() {
   return (
     <section className="flex justify-center items-center h-screen">
-      <div className="w-150 h-72 bg-gradient-to-r  rounded-lg p-8 flex flex-col items-center justify-center items-centerbg-gradient-to-r  from-purple-800 to-purple-950">
+      <div className="w-full h-auto  bg-gradient-to-r p-8 flex flex-col items-center justify-center items-centerbg-gradient-to-r  from-purple-800 to-purple-950 ">
         <h1 className="text-white text-3xl font-normal leading-normal tracking-widest uppercase font-item">
           Are you ready to invest?
         </h1>
@@ -12,15 +12,10 @@ function main() {
 
         <Link
           href="/startPage"
-          className="btn-sm text-white bg-zinc-950 hover:bg-purple-700  px-6 py-2 ml-3 mt-16"
+          className="btn-sm text-white bg-zinc-950 hover:bg-purple-700  px-6 py-2 ml-3 mt-28"
         >
           GET START
         </Link>
-        {/* <button 
-        className="bg-zinc-950 hover:bg-purple-700 text-white font-bold py-2 px-4 mt-14 rounded"
-        >
-          GET START
-        </button> */}
       </div>
     </section>
   );
