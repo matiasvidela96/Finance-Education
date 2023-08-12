cf15402ea07787c4a09f23e097a2a6c36b71277c
Ivo196 <ivo.tonioni@gmail.com>
2023-08-12 13:31:14 +0200

    add email-component

--- app/calculadora/page.tsx
+++ app/calculadora/page.tsx
@@ -1,13 +1,26 @@
-export const metadata = {
-    title: 'Perfil del inversor',
-    description: 'Crea tu perfil de inversion',
-  }
-  
-  import Link from 'next/link'
-  
-  export default function Calculadora() {
-    return (
-     <div>hello from calculadora</div>
-    )
-  }
-  
+"use client";
+
+import Link from "next/link";
+
+export default function Calculadora() {
+  return (
+    <>
+      <div>hello from calculadora</div>
+      <div>
+        <button
+          className="bg-sky-500 text-white p-2 rounded-md"
+          onClick={async () => {
+            const res = await fetch("../api/send", {
+              method: "POST",
+            });
+            const data = await res.json();
+
+            console.log(data);
+          }}
+        >
+          Send Email
+        </button>
+      </div>
+    </>
+  );
+}
