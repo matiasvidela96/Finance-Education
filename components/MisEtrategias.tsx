cf34ee65717e24eb5b167ef015d9835db96f38b7
Ivo196 <ivo.tonioni@gmail.com>
2023-08-29 13:39:42 +0200

    feat MisEstrategias

--- components/MisEtrategias.tsx
+++ components/MisEtrategias.tsx
@@ -2,12 +2,47 @@
 import { useState, useContext } from "react";
 import { PerfilInversorContext } from "../app/contexts/perfilInversor";
 import Icon from "@/components/main-section/icon";
+import Link from "next/link";
 
 function MisEtrategias() {
   const [Income, setIncome] = useState(0);
   const [ShowResult, setShowResult] = useState(false);
   // @ts-ignore
   const { perfilInversor } = useContext(PerfilInversorContext);
+
+  // Mostrar el formulario solo si el perfil del inversor está completado
+  if (!perfilInversor) {
+    return (
+      <div
+        data-aos="fade-up"
+        data-aos-delay="100"
+        data-aos-anchor="[data-aos-id-blocks]"
+      >
+        <div className=" p-6 hover:bg-zinc-700 rounded-lg shadow-md">
+          <h1 className="text-2xl font-bold text-red-600 mb-2">
+            ¡Aún no has completado tu perfil de inversor!
+          </h1>
+          <p className="text-gray-400 mb-5">
+            Por favor, completa tu perfil de inversor para que podamos calcular
+            las mejores estrategias para ti.
+          </p>
+
+          <Icon
+            name="HandCoins"
+            size={48}
+            color={"#f5f5f5"}
+            weight={"light"}
+            title={"Perfil del Inversor"}
+            description={
+              "Contesta las preguntas para definir tu perfil y ver las estrategias que mas se adaptan a ti."
+            }
+            link={"/perfilInversor"}
+          ></Icon>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       <form
@@ -19,53 +54,28 @@ function MisEtrategias() {
       >
         <div className="flex flex-wrap -mx-3 mb-4">
           <div className="w-full px-2">
-            {perfilInversor ? (
-              <h1 className="text-2xl font-bold mb-2">
-                Tu perfil es: {perfilInversor}
-              </h1>
-            ) : (
-              <>
-                {/* Perfil del inversor */}
-
-                <div
-                  data-aos="fade-up"
-                  data-aos-delay="100"
-                  data-aos-anchor="[data-aos-id-blocks]"
-                >
-                  <h1 className="text-xl">
-                    No has realizado tu perfil del inversor (Es necesario para
-                    poder calcular tus estrategias){" "}
-                  </h1>
-                  <Icon
-                    name="HandCoins"
-                    size={48}
-                    color={"#f5f5f5"}
-                    weight={"light"}
-                    title={"Perfil del Inversor"}
-                    description={
-                      "Contesta las preguntas para definir tu perfil y ver las estrategias que mas se adaptan a ti."
-                    }
-                    link={"/perfilInversor"}
-                  ></Icon>
-                </div>
-              </>
-            )}
-
-            <h2 className="text-2xl font-bold mt-3 mb-2">
-              Dinero de Inversion:
-            </h2>
-            <input
-              onChange={(e) => {
-                setShowResult(false);
-                setIncome(Number(e.target.value));
-                console.log(Number(e.target.value));
-              }}
-              id="income"
-              type="number"
-              className="form-input w-full text-gray-300"
-              placeholder="Dinero a invertir (Mayor a $1000)"
-              required
-            />
+            <h1 className="text-2xl font-bold mb-2">
+              Tu perfil es: {perfilInversor}
+            </h1>
+            <div className="mt-8">
+              <label
+                htmlFor="income"
+                className="block text-lg font-semibold text-gray-100 mb-2"
+              >
+                Monto de Inversión:
+              </label>
+              <input
+                onChange={(e) => {
+                  setShowResult(false);
+                  setIncome(Number(e.target.value));
+                }}
+                id="income"
+                type="number"
+                className="form-input w-full p-3 border border-gray-300 rounded-md focus:ring-purple-500 focus:border-purple-500"
+                placeholder="Monto a invertir (Mínimo $1000)"
+                required
+              />
+            </div>
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mt-6">
@@ -80,9 +90,11 @@ function MisEtrategias() {
         </div>
       </form>
       {ShowResult && (
-        <div>
-          <h1>Esto es el resultado</h1>
-          <p>{Income}</p>
+        <div className="mt-5">
+          <h1 className="text-xl font-bold text-gray-400 mb-2">
+            Resultado de la inversión:
+          </h1>
+          <p className="text-lg text-gray-400">{Income}</p>
         </div>
       )}
     </>
