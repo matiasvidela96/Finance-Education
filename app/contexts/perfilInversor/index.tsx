28200eeb99c949d7dc0cdcb87792c67d85d0a93f
Ivo196 <ivo.tonioni@gmail.com>
2023-08-29 10:11:14 +0200

    feat misEstrategias

--- app/contexts/perfilInversor/index.tsx
+++ app/contexts/perfilInversor/index.tsx
@@ -1,14 +1,18 @@
+"use client";
 import { createContext, useState } from "react";
 export const PerfilInversorContext = createContext(null);
 // @ts-ignore
 function Context({ children }) {
-    const [perfilInversor, setPerfilInversor] = useState();
+  const [perfilInversor, setPerfilInversor] = useState();
 
-    return (
-        // @ts-ignore
-        <PerfilInversorContext.Provider value={{ perfilInversor, setPerfilInversor }}>
-            {children}
-        </PerfilInversorContext.Provider>
-    );
+  return (
+    // @ts-ignore
+    <PerfilInversorContext.Provider
+      // @ts-ignore
+      value={{ perfilInversor, setPerfilInversor }}
+    >
+      {children}
+    </PerfilInversorContext.Provider>
+  );
 }
 export default Context;
--- app/misEstrategias/page.tsx
+++ app/misEstrategias/page.tsx
@@ -8,26 +8,28 @@ import BackButton from "@/components/ui/backButton";
 
 export default function misEstrategias() {
   return (
-    <section className="relative">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <BackButton />
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          {/* Page header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 className="h1">Bienvenido a Mis Estrategias</h1>
-            <h3 className="mt-1">
-              "Lugar donde podras encontrar la mejor estrategia en funcion de tu
-              presupuesto y de tu perfil de riesgo"
-            </h3>
-          </div>
+    <>
+      <BackButton />
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="pt-16 pb-12 md:pt-20 md:pb-10">
+            {/* Page header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-10">
+              <h1 className="h1">Bienvenido a Mis Estrategias</h1>
+              <h3 className="mt-1">
+                "Lugar donde podras encontrar la mejor estrategia en funcion de
+                tu presupuesto y de tu perfil de riesgo"
+              </h3>
+            </div>
 
-          {/* Form */}
-          <div className="max-w-sm mx-auto ">
-            <div className="flex items-center my-6"></div>
-            <MisEtrategias />
+            {/* Form */}
+            <div className="max-w-sm mx-auto ">
+              <div className="flex items-center"></div>
+              <MisEtrategias />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
