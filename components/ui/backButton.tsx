0a4e259d2897d1d213ce26ea6efa9bec6f0f5309
matiasvidela96 <matividela@yahoo.com.ar>
2023-09-30 15:38:11 -0300

    prueba bck button

--- components/ui/backButton.tsx
+++ components/ui/backButton.tsx
@@ -3,14 +3,18 @@ import { useRouter } from "next/navigation";
 
 export default function BackButton() {
   const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  };
   return (
     <div>
       <button
         type="button"
         className="bg-purple-600 hover:bg-purple-700 text-white rounded  border-gray-100 py-2 px-3 mt-16"
         onClick={(e) => {
+          handleBack();
           e.stopPropagation();
-          router.back();
+          // router.back();
         }}
       >
         <div className="flex flex-row align-middle">
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -11,15 +11,15 @@ export default function Header() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-20">
           <div className="shrink-0 mr-4">
-            {/* <Link href="/" className="block" aria-label="Cruip"> */}
-            <Image
-              className="max-w-full mx-auto md:max-w-none h-auto mt-8"
-              src={Logo}
-              width={100}
-              height={100}
-              alt="header-Logo"
-            />
-            {/* </Link> */}
+            <Link href="/" className="block" aria-label="Cruip" target="_self">
+              <Image
+                className="max-w-full mx-auto md:max-w-none h-auto mt-8"
+                src={Logo}
+                width={100}
+                height={100}
+                alt="header-Logo"
+              />
+            </Link>
           </div>
 
           {/* Desktop navigation */}
