83d1d99e1018dbf0105a71d80fa24833f597700f
Ivo196 <ivo.tonioni@gmail.com>
2023-09-30 20:16:43 +0200

    feat general 3

--- components/ui/backButton.tsx
+++ components/ui/backButton.tsx
@@ -9,11 +9,9 @@ export default function BackButton() {
         type="button"
         className="bg-purple-600 hover:bg-purple-700 text-white rounded  border-gray-100 py-2 px-3 mt-16"
         onClick={(e) => {
-          e.preventDefault()
-          router.back()
-        }
-        }
-
+          e.stopPropagation();
+          router.back();
+        }}
       >
         <div className="flex flex-row align-middle">
           <svg
