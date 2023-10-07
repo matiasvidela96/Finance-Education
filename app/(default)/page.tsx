978ffeac1ac7c99ee44cfc8101db7151485503d5
Ivo196 <ivo.tonioni@gmail.com>
2023-10-07 12:47:38 +0200

    feat defaultPage

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -13,24 +13,26 @@ const roboto = Roboto({
 });
 function main() {
   return (
-    <section className="flex justify-center text-center items-center h-screen">
-      <div
-        className={`w-full h-auto  bg-gradient-to-r p-8 flex flex-col items-center justify-center items-centerbg-gradient-to-r  from-purple-800 to-purple-950 `}
-      >
-        <h1 className=" text-4xl leading-normal tracking-widest uppercase font-serif">
-          Estas listo para invertir?
-        </h1>
-        <p className="text-gray-200 text-sm mt-3 font-serif">
-          "Explorando un Universo de Conocimiento Financiero: Tu Destino
-          Definitivo para Aprender sobre Inversiones de Manera Asombrosa"
-        </p>
-
-        <a
-          href="/startPage"
-          className="btn-sm text-white bg-zinc-950 hover:bg-purple-700  px-6 py-2 ml-3 mt-16 rounded-xl"
+    <section className="flex-auto flex items-center h-auto justify-center ">
+      <div>
+        <div
+          className={`w-screen  bg-gradient-to-r p-8 flex flex-col items-center justify-center items-centerbg-gradient-to-r  from-purple-800 to-purple-950 `}
         >
-          ADELANTE
-        </a>
+          <h1 className=" text-4xl leading-normal tracking-widest uppercase font-serif">
+            Estas listo para invertir?
+          </h1>
+          <p className="text-gray-200 text-sm mt-3 font-serif">
+            "Explorando un Universo de Conocimiento Financiero: Tu Destino
+            Definitivo para Aprender sobre Inversiones de Manera Asombrosa"
+          </p>
+
+          <a
+            href="/startPage"
+            className="btn-sm text-white bg-zinc-950 hover:bg-purple-700  px-6  ml-3 mt-14 rounded-xl"
+          >
+            ADELANTE
+          </a>
+        </div>
       </div>
     </section>
   );
